790d4768f95cbddd9bdffa2513770c0b87b6b8a1
omiend <omiend@gmail.com>
2014-10-30 14:28:58 +0900

    Twitter4jによるOAuth認証作成

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
 
+import play.api.cache._
+
+import twitter4j._
+
+/** Applocation Controller */
 object Application extends Controller {
 
+  /** index画面起動 */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  	// CacheからTwitter4j.Userオブジェクトを取得し、index画面を機動
+  	Ok(views.html.index(Cache.getAs[User]("twitter_user")))
   }
 
 }
